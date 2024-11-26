22ede75d903e42259d93c3b1f9f17758248a20cc
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-11-25 23:59:50 -0300

    Novas implementações e correções

--- app/src/main/java/com/femuniz/clientlist/activity/AddClientActivity.java
+++ app/src/main/java/com/femuniz/clientlist/activity/AddClientActivity.java
@@ -3,11 +3,13 @@ package com.femuniz.clientlist.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 
 import com.femuniz.clientlist.R;
 import com.femuniz.clientlist.Utils.BaseActivity;
+import com.femuniz.clientlist.Utils.UtilitysActivity;
 
 public class AddClientActivity extends BaseActivity {
     @Override
@@ -15,6 +17,15 @@ public class AddClientActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_client);
+
+        EditText inputCpfClient = findViewById(R.id.inputCpfClient);
+        EditText inputPhoneClient = findViewById(R.id.inputPhoneClient);
+        EditText inputBirthDateClient = findViewById(R.id.inputBirthDateClient);
+        EditText inputCpeClient = findViewById(R.id.inputCpeClient);
+        UtilitysActivity.applyMask(inputCpfClient, "###.###.###-##");
+        UtilitysActivity.applyMask(inputPhoneClient, "(##)#####-####");
+        UtilitysActivity.applyMask(inputBirthDateClient, "##/##/####");
+        UtilitysActivity.applyMask(inputCpeClient, "#####-###");
     }
 
     public void AddClient_Click(View view){
